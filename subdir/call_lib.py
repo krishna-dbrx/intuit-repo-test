57cb74ca4d36cfe1121b4081a1bea66e26579491
krishna.satyavarapu@databricks.com <krishna.satyavarapu@databricks.com>
2022-06-24 22:38:14 +0000

    adding more

---
+++ subdir/call_lib.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+# COMMAND ----------
+
+from mainnotebook import checkLibarary
+checkLibarary()
+
+# COMMAND ----------
+
+
